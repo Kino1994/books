4a0448fb58ceb7b0e0003295b953e64747a383a4
Kino1994 <jdevicente@genasys.com>
2020-12-08 00:06:43 +0100

    First commit

---
+++ src/test/java/es/joaquin/books/BooksApplicationTests.java
@@ -0,0 +1,13 @@
+package es.joaquin.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
